726047a4458f043ba458f9450b920c24957cfa2e
julee <julee.vv@outlook.com>
2016-04-15 15:08:16 +0800

    get_property not allowed in new cmake version

--- AndroidNdkGdb.cmake
+++ AndroidNdkGdb.cmake
@@ -57,7 +57,7 @@ macro(android_ndk_gdb_enable)
 
         set(NDK_GDB_SOLIB_PATH ${ANDROID_PROJECT_DIR}/obj/local/${ANDROID_NDK_ABI_NAME}/)
         file(MAKE_DIRECTORY ${NDK_GDB_SOLIB_PATH})
-        
+
         # 1. generate essential Android Makefiles
         file(MAKE_DIRECTORY ${ANDROID_PROJECT_DIR}/jni)
         if(NOT EXISTS ${ANDROID_PROJECT_DIR}/jni/Android.mk)
@@ -71,6 +71,7 @@ macro(android_ndk_gdb_enable)
         get_directory_property(PROJECT_INCLUDES DIRECTORY ${PROJECT_SOURCE_DIR} INCLUDE_DIRECTORIES)
         string(REGEX REPLACE ";" " " PROJECT_INCLUDES "${PROJECT_INCLUDES}")
         file(WRITE ${LIBRARY_OUTPUT_PATH}/gdb.setup "set solib-search-path ${NDK_GDB_SOLIB_PATH}\n")
+        file(APPEND ${LIBRARY_OUTPUT_PATH}/gdb.setup "source ${ANDROID_NDK}/prebuilt/common/gdb/common.setup\n")
         file(APPEND ${LIBRARY_OUTPUT_PATH}/gdb.setup "directory ${PROJECT_INCLUDES}\n")
     
         # 3. copy gdbserver executable
@@ -82,15 +83,15 @@ endmacro()
 # copies the debug version to NDK_GDB_SOLIB_PATH then strips symbols of original
 macro(android_ndk_gdb_debuggable TARGET_NAME)
     if(ANDROID)
-        get_property(TARGET_LOCATION TARGET ${TARGET_NAME} PROPERTY LOCATION)
+        # get_property(TARGET_LOCATION TARGET ${TARGET_NAME} PROPERTY LOCATION)
         
         # create custom target that depends on the real target so it gets executed afterwards
         add_dependencies(NDK_GDB ${TARGET_NAME})
     
         # 4. copy lib to obj
-        add_custom_command(TARGET NDK_GDB POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy_if_different ${TARGET_LOCATION} ${NDK_GDB_SOLIB_PATH})
+        add_custom_command(TARGET NDK_GDB POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy_if_different $<TARGET_FILE:${TARGET_NAME}> ${NDK_GDB_SOLIB_PATH})
     
         # 5. strip symbols
-        add_custom_command(TARGET NDK_GDB POST_BUILD COMMAND ${CMAKE_STRIP} ${TARGET_LOCATION})
+        add_custom_command(TARGET NDK_GDB POST_BUILD COMMAND ${CMAKE_STRIP} $<TARGET_FILE:${TARGET_NAME}>)
     endif()
 endmacro()
